6b9a2c1c3cb8427a44a9bb3bc17a3960370394f2
vivi-learn <skek090000@gmail.com>
2025-10-15 20:51:36 +0900

    Refactor code structure for improved readability and maintainability

---
+++ main.py
@@ -0,0 +1,43 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+
+
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"],
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"],
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"],
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+
+TranslatorCrew().assemble_crew().kickoff(
+    inputs={
+        "sentence": "I'm vivi and I like to ride my bicicle in Korea",
+    }
+)
