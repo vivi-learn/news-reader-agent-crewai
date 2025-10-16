1734a5d4b82c260922024cceb81873ced9792f56
vivi-learn <skek090000@gmail.com>
2025-10-16 11:22:26 +0900

    Refactor agent and task configurations to enhance news processing capabilities and improve code structure

--- main.py
+++ main.py
@@ -8,50 +8,45 @@ from tools import count_letters
 
 
 @CrewBase
-class TranslatorCrew:
+class NewsReaderAgent:
 
     @agent
-    def translator_agent(self):
-        return Agent(
-            config=self.agents_config["translator_agent"],
-        )
-    
+    def news_hunter_agent(self):
+        return Agent(config=self.agents_config["news_hunter_agent"])
+
     @agent
-    def counter_agent(self):
-        return Agent(
-            config=self.agents_config["counter_agent"],
-            tools=[count_letters]
-        )  
+    def summarizer_agent(self):
+        return Agent(config=self.agents_config["summarizer_agent"])
+
+    @agent
+    def curator_agent(self):
+        return Agent(config=self.agents_config["curator_agent"])
+
 
     @task
-    def translate_task(self):
+    def content_harvesting_task(self):
         return Task(
-            config=self.tasks_config["translate_task"],
-        )
-
-    @task
-    def retranslate_task(self):
-        return Task(
-            config=self.tasks_config["retranslate_task"],
+            config=self.tasks_config["content_harvesting_task"],
         )
     
     @task
-    def count_task(self):
+    def summarization_task(self):
         return Task(
-            config=self.tasks_config["count_task"],
+            config=self.tasks_config["summarization_task"],
         )
-
+    
+    @task
+    def final_report_assembly_task(self):
+        return Task(
+            config=self.tasks_config["final_report_assembly_task"],
+        )
+    
     @crew
-    def assemble_crew(self):
+    def crew(self):
         return Crew(
-            agents=self.agents,
             tasks=self.tasks,
+            agents=self.agents,
             verbose=True,
         )
 
-
-TranslatorCrew().assemble_crew().kickoff(
-    inputs={
-        "sentence": "I'm vivi and I like to ride my bicicle in Korea",
-    }
-)
+NewsReaderAgent().crew().kickoff()
